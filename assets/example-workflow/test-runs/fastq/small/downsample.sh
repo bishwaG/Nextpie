fd48bcaf37016e55552d7d2aef87729eba6bfadd
Bishwa Ghimire <bishwa.ghimire@helsinki.fi>
2024-07-12 15:33:55 +0300

    Remove files and folders 	deleted:    example-workflow/test-runs/.nextflow.log 	deleted:    example-workflow/test-runs/.nextflow.log.1 	deleted:    example-workflow/test-runs/.nextflow.log.2 	deleted:    example-workflow/test-runs/.nextflow.log.3 	deleted:    example-workflow/test-runs/.nextflow.log.4 	deleted:    example-workflow/test-runs/.nextflow.log.5 	deleted:    example-workflow/test-runs/.nextflow.log.6 	deleted:    example-workflow/test-runs/.nextflow.log.7 	deleted:    example-workflow/test-runs/.nextflow.log.8 	deleted:    example-workflow/test-runs/.nextflow.log.9 	deleted:    example-workflow/test-runs/.nextflow/cache/6f43f46f-8d94-4e03-90a5-d454477fd40d/db/000003.log 	deleted:    example-workflow/test-runs/.nextflow/cache/6f43f46f-8d94-4e03-90a5-d454477fd40d/db/CURRENT 	deleted:    example-workflow/test-runs/.nextflow/cache/6f43f46f-8d94-4e03-90a5-d454477fd40d/db/LOCK 	deleted:    example-workflow/test-runs/.nextflow/cache/6f43f46f-8d94-4e03-90a5-d454477fd40d/db/MANIFEST-000002 	deleted:    example-workflow/test-runs/.nextflow/cache/6f43f46f-8d94-4e03-90a5-d454477fd40d/index.loving_bhaskara 	deleted:    example-workflow/test-runs/.nextflow/history 	deleted:    example-workflow/test-runs/fastq/small/downsample.sh

--- assets/example-workflow/test-runs/fastq/small/downsample.sh
+++
@@ -1,4 +0,0 @@
-seqtk sample -s100 ../big/SRR2121687_S1_R1_001.fastq.gz 5000 | gzip > SRR2121687_S1_R1_001.fastq.gz
-seqtk sample -s100 ../big/SRR2121687_S1_R2_001.fastq.gz 5000 | gzip > SRR2121687_S1_R2_001.fastq.gz 
-seqtk sample -s100 ../big/SRR2121688_S2_R1_001.fastq.gz 5000 | gzip > SRR2121688_S2_R1_001.fastq.gz
-seqtk sample -s100 ../big/SRR2121688_S2_R2_001.fastq.gz 5000 | gzip > SRR2121688_S2_R2_001.fastq.gz
