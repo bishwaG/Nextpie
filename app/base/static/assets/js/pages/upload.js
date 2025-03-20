270c9daf15bf26394e46bfab5251026e6907fdbc
Bishwa Ghimire <bishwa.ghimire@helsinki.fi>
2025-03-20 17:44:36 +0200

    More logics added for newer text fields 	modified:   app/base/static/assets/js/pages/upload.js

--- app/base/static/assets/js/pages/upload.js
+++ app/base/static/assets/js/pages/upload.js
@@ -1,20 +1,68 @@
+
+// form reset ------------------------------------------------------------------
+document.getElementById("reset-button").addEventListener("click", function() {
+        //if (confirm("Are you sure you want to reset the form?")) {
+            document.getElementById("form_upload").reset();
+        //}
+    });
+
+
+// function to remove texts ----------------------------------------------------
+const fileInput = document.getElementById("fileInput");
+const textInputs = document.querySelectorAll("input[type='text']");
+
+// Function to clear content letter by letter with flashing effect
+function clearContentWithFlashing(inputField) {
+    let currentValue = inputField.value;
+    let index = currentValue.length;
+    let isOrange = false;  // Toggle for flashing effect
+
+    // Start flashing effect
+    const flashInterval = setInterval(() => {
+        inputField.style.backgroundColor = isOrange ? "white" : "orange"; // Toggle between red and white
+        isOrange = ! isOrange;
+    }, 250); // Flash every x ms
+
+    // Interval to remove one character at a time
+    const textRemoveInterval = setInterval(() => {
+        if (index > 0) {
+            inputField.value = currentValue.substring(0, index - 1); // Remove one letter
+            index--;
+        } else {
+            clearInterval(textRemoveInterval); // Stop text removal
+            clearInterval(flashInterval); // Stop flashing effect
+            inputField.style.backgroundColor = "white"; // Reset background to normal
+        }
+    }, 300); // Adjust this to control the speed (milli seconds)
+}
+
+// document ready function
 $(document).ready(function() {
     
-    //$('#login').click(function() {
+    //submit
     $('form').submit(function (e) {
     
+        
         var form_data = new FormData();
-        form_data.append('myfile', $('#reportFile').prop('files')[0]);  
-	
-	
-	// for trace files
-	var ins = document.getElementById('traceFiles').files.length;		
-	for (var x = 0; x < ins; x++) {
-		//form_data.append("myfile2[]", document.getElementById('traceFiles').files[x]);
-		form_data.append('myfile2[]', $('#traceFiles').prop('files')[x]);
+        form_data.append("workflowName", $("#workflowName").val());
+        form_data.append("workflowVer", $("#workflowVer").val());
+        form_data.append("groupName", $("#groupName").val());
+        form_data.append("projectName", $("#projectName").val());
+        
+        if($("#reportFile")[0].files.length >0){
+        	form_data.append('reportFile', $('#reportFile').prop('files')[0]);  
+		textInputs.forEach(input => {
+                clearContentWithFlashing(input);  // Clear content of each input field letter by letter
+            });
 	}
 	
-				
+	// for trace files
+	var ins = document.getElementById('traceFiles').files.length;
+	for (var x = 0; x < ins; x++) {
+		//form_data.append("myfile2[]", document.getElementById('traceFiles').files[x]);
+		form_data.append('traceFile[]', $('#traceFiles').prop('files')[x]);
+	}
+	
         $.ajax({
             xhr: function() {
 		var xhr = new window.XMLHttpRequest();
@@ -30,6 +78,7 @@ $(document).ready(function() {
 		        $('#progressbar').val(percentComplete)
 		        $('.percent').html(percentVal)
 		    }
+
 		}, false);
 		return xhr;
 	    },
@@ -47,13 +96,37 @@ $(document).ready(function() {
             //},
             success: function(data)
             {
-                 data = JSON.stringify(data)
+                 //data = JSON.stringify(data)
+                 console.log("AJAX success!"); 
                  console.log(data); 
                  $('#message').show();
-                 $("#message").html(data);
+                 $("#message").html(data.message);
 		 //$('#form_upload').trigger("reset");
 		 //$('#progressbar').val(0)
 		 //$('.percent').html(0)
+		 
+		
+		const messageDiv = $("#message");
+		const messageText = $("#message-text");
+
+		messageText.html(data);
+		
+
+		if (data.response === "warning") {
+		  messageDiv.removeClass("alert-info alert-success alert-danger alert-warning");
+		  messageDiv.addClass("alert alert-warning alert-dismissible fade show");
+		} else if (data.response === "success") {
+		  messageDiv.removeClass("alert-info alert-success alert-danger alert-warning");
+		  messageDiv.addClass("alert alert-success alert-dismissible fade show");
+		} else if (data.response === "error") {
+		  messageDiv.removeClass("alert-info alert-success alert-danger alert-warning");
+		  messageDiv.addClass("alert alert-danger alert-dismissible fade show");
+		}
+
+		messageDiv.fadeIn();
+		 
+		 
+            
             },
             complete: function(xhr, textStatus) 
             {
@@ -73,7 +146,7 @@ $(document).ready(function() {
 
 
 
-// Inject our CSRF token into our AJAX request.
+	// Inject our CSRF token into our AJAX request.
         $.ajaxSetup({
             beforeSend: function(xhr, settings) {
                 if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
@@ -82,3 +155,61 @@ $(document).ready(function() {
             }
         })
     });
+
+
+        
+// for displaying report file after loading a file.
+document.getElementById("reportFile").addEventListener("change", function(event) {
+    const file = event.target.files[0]; // Get the selected file
+
+    if (file) {   	
+    	textInputs.forEach(input => {
+                clearContentWithFlashing(input);  // Clear content of each input field letter by letter
+            });
+    	
+        const reader = new FileReader();
+
+        // Define what happens when reading is complete
+        reader.onload = function(e) {
+            document.getElementById("file-header-1").textContent = "Report file : " + file.name;
+            document.getElementById("output-1").textContent = e.target.result; // Display file content
+            document.getElementById("report-file-view").style.display = "block";
+        };
+
+        reader.onerror = function() {
+            console.error("Error reading file!");
+        };
+
+        reader.readAsText(file); // Read file as text
+    }
+});
+
+
+
+// for displaying trace file after loading a file.
+document.getElementById("traceFiles").addEventListener("change", function(event) {
+    const file = event.target.files[0]; // Get the selected file
+
+    if (file) {
+        const reader = new FileReader();
+
+        // Define what happens when reading is complete
+        reader.onload = function(e) {
+            const fileContent = e.target.result; // File content as text
+            const lines = fileContent.split("\n"); // Split the content by line
+            // Get the first 10 lines
+            const first10Lines = lines.slice(0, 10).join("\n");
+
+            let file = event.target.files[0];
+            document.getElementById("file-header-2").textContent = "Trace file (first 10 lines) : " + file.name;
+            document.getElementById("output-2").textContent = first10Lines; // Display file content
+            document.getElementById("trace-file-view").style.display = "block";
+        };
+
+        reader.onerror = function() {
+            console.error("Error reading file!");
+        };
+
+        reader.readAsText(file); // Read file as text
+    }
+});
