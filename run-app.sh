5f17d9e375fc0c5375b93bb64b14c87bdbd88862
Bishwa Ghimire <bishwa.ghimire@helsinki.fi>
2025-03-24 15:04:25 +0200

    ENV vars fixed 	modified:   run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 source env/bin/activate
-export FLASK_APP=run.py
-export FLASK_ENV=development
+#export FLASK_APP=run.py
+#export FLASK_ENV=development
+export FLASK_DEBUG=1
+export FLASK_APP=run.py 
+
 
 flask run --debugger
