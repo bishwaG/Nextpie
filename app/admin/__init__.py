61a0cf7f51dc085ece22565364cbc6e44e0649c0
Bishwa Ghimire <bishwa.ghimire@helsinki.fi>
2025-03-24 15:17:17 +0200

    Template admin/index.html added 	modified:   app/admin/__init__.py

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from flask import Flask
+from flask import Flask, redirect, request, url_for, render_template
 from flask_admin import Admin
 from app import app, db
 from app.base.models import User
@@ -12,20 +12,24 @@ from app.admin.models import MyModelAdminUser, MyModelAdminGroup, MyModelAdminPr
 from app.admin.models import DashboardView
 
 from flask_admin.menu import MenuLink
+
+## admin views
+from app.admin.adminviews import MyHomeView
 ## Flask-admin
 admin = Admin(app, 
               name='Admin Dashboard', 
-              template_mode='bootstrap3', index_view=DashboardView())
+              template_mode='bootstrap3', 
+              index_view=MyHomeView( name="Home", template='admin/index.html', url="/admin")
+              )
 
 ## add DB tabel views
 admin.add_view(MyModelAdminUser(User, db.session))
 admin.add_view(MyModelAdminGroup(Group, db.session))
+
 admin.add_view(MyModelAdminProject(Project, db.session))
 admin.add_view(MyModelAdminRun(Run, db.session))
 admin.add_view(MyModelAdminProcess(Process, db.session))
 admin.add_link(MenuLink(name='Logout', url='/logout'))
-
-
 
 blueprint = Blueprint(
     'admin_blueprint',
@@ -34,6 +38,4 @@ blueprint = Blueprint(
     template_folder='templates',
     static_folder='static'
 )
-
-
 
