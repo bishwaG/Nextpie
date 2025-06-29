ebc8e12d4e79a851bd21e724c3be319c60923871
Bishwa Ghimire <bishwa.ghimire@helsinki.fi>
2025-06-29 16:20:37 +0300

    Version changed and added test repo URL 	modified:   assets/example-workflow/test-runs/run-analysis.sh

--- assets/example-workflow/test-runs/run-analysis.sh
+++ assets/example-workflow/test-runs/run-analysis.sh
@@ -4,8 +4,9 @@
 NXF_OPTS='-Xms1g -Xmx1g'
 _JAVA_OPTIONS='-Xms1g -Xmx4g'
 
+export NXF_PLUGINS_TEST_REPOSITORY=https://github.com/bishwaG/nf-nextpie/releases/download/0.0.2/nf-nextpie-0.0.2-meta.json
 
-./nextflow run ../main.nf -plugins nf-nextpie@0.0.1 \
+./nextflow run ../main.nf -plugins nf-nextpie@0.0.2 \
   --fastqs 'fastq/*_R{1,2}*.fastq.gz' \
   --name "test_project" \
   --group "test_research_group"
