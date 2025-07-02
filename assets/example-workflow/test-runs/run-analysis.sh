589baa5ee8111c8ddaba50905485ba70d3a6acc0
Bishwa Ghimire <bishwa.ghimire@helsinki.fi>
2025-07-02 12:00:03 +0300

    Env var NXF_PLUGINS_TEST_REPOSITORY removed 	modified:   run-analysis.sh

--- assets/example-workflow/test-runs/run-analysis.sh
+++ assets/example-workflow/test-runs/run-analysis.sh
@@ -4,8 +4,6 @@
 NXF_OPTS='-Xms1g -Xmx1g'
 _JAVA_OPTIONS='-Xms1g -Xmx4g'
 
-export NXF_PLUGINS_TEST_REPOSITORY=https://github.com/bishwaG/nf-nextpie/releases/download/0.0.2/nf-nextpie-0.0.2-meta.json
-
 ./nextflow run ../main.nf -plugins nf-nextpie@0.0.2 \
   --fastqs 'fastq/*_R{1,2}*.fastq.gz' \
   --name "test_project" \
